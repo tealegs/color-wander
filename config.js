efee7375bfc4c822004ffeb371ce11d2197a0df0
Tom Legnard <tomlegnard@toms-mbp.home>
2020-06-15 23:20:25 -0400

    adding BLM image and tweaking code

--- config.js
+++ config.js
@@ -12,14 +12,16 @@ module.exports = function (seed) {
   var randomFunc = seedRandom(seed);
   var random = createRandomRange(randomFunc);
 
-  var maps = [
-    'sym6.jpg', 'sym3.jpg',
-    'scifi.jpg', 'nature1.jpg',
-    'map7.jpg', 'geo5.jpg', 'geo4.jpg',
-    'geo3.jpg', 'geo1.jpg', 'fractal2.jpg',
-    'fractal1.jpg', 'eye.jpg', 'city5.jpg',
-    'city2.jpg', 'church2.jpg', 'architecture.jpg',
-    'pat1.jpg'
+  // var maps = [
+  //   'sym6.jpg', 'sym3.jpg',
+  //   'scifi.jpg', 'nature1.jpg',
+  //   'map7.jpg', 'geo5.jpg', 'geo4.jpg',
+  //   'geo3.jpg', 'geo1.jpg', 'fractal2.jpg',
+  //   'fractal1.jpg', 'eye.jpg', 'city5.jpg',
+  //   'city2.jpg', 'church2.jpg', 'architecture.jpg',
+  //   'pat1.jpg'
+
+  var maps = ['blm2.jpeg'
   ].map(function (p) {
     return 'maps/' + p;
   });
@@ -27,23 +29,23 @@ module.exports = function (seed) {
   var mapSrc = maps[Math.floor(random(maps.length))];
 
   return {
-    // rendering options
+    // rendering options  
     random: randomFunc,
     seedName: seed,
     pointilism: random(0, 0.1),
     noiseScalar: [ random(0.000001, 0.000001), random(0.0002, 0.004) ],
     globalAlpha: 0.5,
     startArea: random(0.0, 1.5),
-    maxRadius: random(5, 100),
+    maxRadius: random(5, 75),
     lineStyle: random(1) > 0.5 ? 'round' : 'square',
     interval: random(0.001, 0.01),
     count: Math.floor(random(50, 2000)),
-    steps: Math.floor(random(100, 1000)),
+    steps: Math.floor(random(500, 2000)),
     endlessBrowser: false, // Whether to endlessly step in browser
 
     // background image that drives the algorithm
     debugLuma: false,
-    backgroundScale: 1,
+    backgroundScale: .5,
     backgorundFille: 'black',
     backgroundSrc: mapSrc,
 
